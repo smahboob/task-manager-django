a0a743f833b0b9faaf162e533db5a7f7b42cfaa9
Saad Mahboob <smahboob@fandm.edu>
2021-05-19 10:48:54 +0500

    Set up registeration

--- task_app/urls.py
+++ task_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import CustomLoginView, TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, RegisterPage
 from django.contrib.auth.views import LogoutView
 
 #This is where we add all the urls for our applications
 urlpatterns = [
     path("login/", CustomLoginView.as_view(), name="login"),
+    path('register/', RegisterPage.as_view(), name='register'),
+    
     path("", TaskList.as_view() , name="tasks"),
     path("task/<int:pk>/", TaskDetail.as_view() , name="detail-task"),
     path("create-task/", TaskCreate.as_view() , name="create-task"),
--- task_app/views.py
+++ task_app/views.py
@@ -1,20 +1,40 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 
 #login view 
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import redirect
 
 from django.urls import reverse_lazy
 from .models import Task
-
+from django import forms
 
 class CustomLoginView(LoginView):
 	template_name = "task_app/login.html"
 	fields = '__all__'
-	redirect_authenticated_user = False
+	redirect_authenticated_user = True
+ 
+class RegisterPage(FormView):
+    template_name = 'task_app/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
  
 class TaskList(ListView):
 	model = Task
